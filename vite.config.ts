113bd200ca7c7886030917d506da7a0cd0d9dbb7
MosPTS <mospersonalcoaching@gmail.com>
2025-10-23 12:10:27 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/mos-studio-web/' : '/', // GitHub Pages için base path
+  base: '/mos-studio-web/', // GitHub Pages için base path
   server: {
     host: "::",
     port: 8080,
